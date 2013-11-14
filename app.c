fac83a666a6ede954c2bae43cb36762104701556
Amal Cao <amalcaowei@gmail.com>
2013-11-14 16:17:57 +0800

    Delete the old code in app.c

--- app.c
+++ app.c
@@ -6,32 +6,6 @@
 #include "thread.h"
 #include "channel.h"
 
-# if 0
-void sub_task(void *id)
-{
-	int i, j;
-	for (i=0; i<10; ++i) {
-		printf("Hello ! my id is %d!\n", (int)id);
-        for (j=0; j<10000000; ++j);
-#ifndef ENABLE_TIMER
-		thread_yeild();
-#endif
-	}
-	
-	thread_exit(0);
-}
-
-int user_main (void* id)
-{	
-	int i;
-	for (i=1; i<100; i++) {
-	    thread_t thread = thread_allocate (sub_task, i, "", TSC_THREAD_NORMAL, 0);
-    }
-	sub_task (0);
-}
-
-# endif
-
 void sub_task (void * arg)
 {
     thread_t parent = (thread_t)arg;
