fff82550a816dcefe1b553e5c567ae8ed38f09e1
Amal Cao <amalcaowei@gmail.com>
2013-11-18 15:38:50 +0800

    Add a new example to test the time-sharing mechanism.

---
+++ prio.c
@@ -0,0 +1,57 @@
+/* - - - - 
+ * The User APP using LibTSC ..
+ * - - - - */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include "thread.h"
+#include "channel.h"
+
+void sub_task (void * arg)
+{
+    thread_t parent = (thread_t)arg;
+    int id, i;
+    size_t size = sizeof (int);
+
+    recv (parent, &size, &id, true);
+    // printf ("[sub_task:] recv id is %d!\n", id);
+
+    for (i=0; i<50000000; i++);
+
+    send (parent, sizeof(int), &id);
+
+    thread_exit (0);
+}
+
+
+int user_main (void * arg)
+{
+    thread_t threads[100];
+    thread_attributes_t attr;
+    thread_attr_init (& attr);
+        // thread_attr_set_stacksize (& attr, 8*1024*1024); // 4MB stack
+
+    int i;
+    for (i = 0; i<100; ++i) {
+        if (i % 2) thread_attr_set_timeslice (& attr, 2);
+        else thread_attr_set_timeslice (& attr, 4);
+
+        threads[i] = thread_allocate (sub_task, thread_self(), "", TSC_THREAD_NORMAL, & attr);
+    }
+
+    for (i = 0; i<100; ++i) {
+        send (threads[i], sizeof(int), &i);
+    }
+
+    for (i=0; i< 100; ++i) {
+        int id;
+        size_t size = sizeof (int);
+        recv (NULL, &size, &id,  true);
+        printf ("[main_task:] recv id is %d!\n", id);
+    }
+
+    thread_exit (0);
+
+    return 0;
+}
