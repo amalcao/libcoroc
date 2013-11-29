a8c85abb56035b5a837f23ee8830f9a88d83ab56
Amal Cao <amalcaowei@gmail.com>
2013-11-29 11:40:03 +0800

    Using the spinlock to replace the mutex lock.

--- lock.h
+++ lock.h
@@ -2,23 +2,26 @@
 #define _TSC_SUPPORT_LOCK_H_
 
 #include <pthread.h>
+#if defined(__APPLE__)
+#include "pthread_spinlock.h"
+#endif
 
-typedef pthread_mutex_t * lock_t;
+typedef pthread_spinlock_t * lock_t;
 
 static inline lock_t lock_allocate (void)
 {
-    lock_t lock = malloc (sizeof(pthread_mutex_t));
-    pthread_mutex_init (lock, NULL);
+    lock_t lock = malloc (sizeof(pthread_spinlock_t));
+    pthread_spin_init (lock, PTHREAD_PROCESS_PRIVATE);
     return lock;
 }
 
 static inline void lock_deallocate (lock_t lock)
 {
-    pthread_mutex_destroy(lock);
+    pthread_spin_destroy(lock);
     free (lock);
 }
 
-#define lock_acquire pthread_mutex_lock
-#define lock_release pthread_mutex_unlock
+#define lock_acquire pthread_spin_lock
+#define lock_release pthread_spin_unlock
 
 #endif // _TSC_SUPPORT_LOCK_H_
