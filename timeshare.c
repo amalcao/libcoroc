5b269a9fc0dd99b0bf9e1f1fac926102624b957f
Amal Cao <amalcaowei@gmail.com>
2013-11-29 11:38:03 +0800

    Add a new example to test the Time-Sharing mechanism.

---
+++ timeshare.c
@@ -0,0 +1,37 @@
+/* - - - - 
+ * The User APP using LibTSC ..
+ * - - - - */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include "thread.h"
+#include "channel.h"
+
+void sub_task (void * arg)
+{
+    uint64_t id = (uint64_t)arg;
+
+    printf ("[sub_task:] id is %i!\n", id);
+
+    while (1);
+
+    thread_exit (0);
+}
+
+
+int user_main (void * arg)
+{
+    thread_t threads[100];
+
+    int i;
+    for (i = 0; i<100; ++i) {
+        threads[i] = thread_allocate (sub_task, i, "", TSC_THREAD_NORMAL, 0);
+    }
+
+    while (1);
+
+    thread_exit (0);
+
+    return 0;
+}
