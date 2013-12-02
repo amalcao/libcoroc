c39410ee70c66613dbcc35994f97873f61587ee6
Amal Cao <amalcaowei@gmail.com>
2013-12-02 14:28:15 +0800

    Bug fix

--- lock.h
+++ lock.h
@@ -10,8 +10,8 @@ typedef pthread_spinlock_t lock;
 typedef lock * lock_t;
 
 #define lock_init(lock) pthread_spin_init(lock, PTHREAD_PROCESS_PRIVATE)
-#define lock_acquire(lock) pthread_spin_lock(lock)
-#define lock_release(lock) pthread_spin_unlock(lock)
-#define lock_finit(lock) pthread_spin_destroy(lock)
+#define lock_acquire pthread_spin_lock
+#define lock_release pthread_spin_unlock
+#define lock_fini pthread_spin_destroy
 
 #endif // _TSC_SUPPORT_LOCK_H_
