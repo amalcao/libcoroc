034b13f127c5785035796f056f87c581a377ab79
Amal Cao <amalcaowei@gmail.com>
2013-12-13 11:28:36 +0800

    Bug fix..

--- clock.c
+++ clock.c
@@ -3,18 +3,16 @@
 #include "clock.h"
 #include "vpu.h"
 
-#define TSC_CLOCK_PERIOD_NANOSEC    500000
+#define TSC_CLOCK_PERIOD_NANOSEC    500000  // 0.5 ms per signal
 
 clock_manager_t clock_manager;
 
 void clock_initialize (void)
 {
 #ifdef ENABLE_TIMER
-    sigset_t sigmask;
     struct sigaction act;
 
-    sigemptyset (&sigmask);
-    sigaddset (&sigmask, TSC_CLOCK_SIGNAL);
+    sigaddset (& act . sa_mask, TSC_CLOCK_SIGNAL);
 
     act . sa_handler = vpu_clock_handler;
     act . sa_flags = SA_RESTART;
@@ -25,11 +23,11 @@ void clock_initialize (void)
 
 void clock_routine (void)
 {
-    struct timespec period = {0, TSC_CLOCK_PERIOD_NANOSEC};
     sigset_t sigmask;
     sigfillset (&sigmask);
 
     while (true) {
+        struct timespec period = {0, TSC_CLOCK_PERIOD_NANOSEC};
         pselect (0, NULL, NULL, NULL, &period, &sigmask);
 
 #ifdef ENABLE_TIMER
