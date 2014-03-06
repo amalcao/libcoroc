18047a07f5a751a8d226b5f509533dfe958bc8dd
Amal Cao <amalcaowei@gmail.com>
2014-03-06 10:35:30 +0800

    Add a new module for handling the asynchronized socket IO, the idea is from the GoLang's `netpoll' mechanism..

---
+++ netpoll.h
@@ -0,0 +1,41 @@
+#ifndef _TSC_NETPOLL_H_
+#define _TSC_NETPOLL_H_
+
+#include <stdbool.h>
+
+#include "support.h"
+#include "lock.h"
+#include "thread.h"
+
+enum {
+    TSC_NETPOLL_READ = 1,
+    TSC_NETPOLL_WRITE,
+};
+
+typedef struct tsc_poll_desc {
+    bool        done;
+    bool        ok;
+    int         fd;
+    int         mode;
+    int         id; // for fast delete ...
+    thread_t    wait;
+    lock        lock;
+} *tsc_poll_desc_t;
+
+// internal netpoll API..
+int __tsc_netpoll_init (int max);
+int __tsc_netpoll_add (tsc_poll_desc_t desc);
+int __tsc_netpoll_rem (tsc_poll_desc_t desc);
+int __tsc_netpoll_fini (void);
+bool __tsc_netpoll_polling (bool block);
+
+void tsc_netpoll_initialize (void);
+int tsc_netpoll_wakeup (tsc_poll_desc_t desc, bool ok);
+
+// the public netpoll API ..
+int tsc_net_nonblock (int fd);
+int tsc_net_read (int fd, void *buf, int size);
+int tsc_net_write (int fd, void *buf, int size);
+int tsc_net_wait (int fd, int mode);
+
+#endif // _TSC_NETPOLL_H_
---
+++ netpoll_epoll.c
@@ -0,0 +1,82 @@
+#include <sys/epoll.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <assert.h>
+
+#include "netpoll.h"
+
+
+static int __tsc_epfd = -1;
+
+int __tsc_netpoll_init (int max)
+{
+  __tsc_epfd = epoll_create(max);
+  return __tsc_epfd;
+}
+
+int __tsc_netpoll_fini (void)
+{
+  // TODO
+  return 0;
+}
+
+static inline int __tsc_netpoll_ctl (tsc_poll_desc_t desc, int op)
+{
+  struct epoll_event ev;
+  ev.events = EPOLLET;
+
+  if (desc -> mode & TSC_NETPOLL_READ)
+    ev.events |= (EPOLLIN|EPOLLRDHUP);
+  if (desc -> mode & TSC_NETPOLL_WRITE)
+    ev.events |= EPOLLOUT;
+
+  ev.data.ptr = desc;
+
+  return epoll_ctl (__tsc_epfd, op, desc -> fd, & ev);
+}
+
+int __tsc_netpoll_add (tsc_poll_desc_t desc)
+{
+  return __tsc_netpoll_ctl (desc, EPOLL_CTL_ADD);
+}
+
+int __tsc_netpoll_rem (tsc_poll_desc_t desc)
+{
+  return __tsc_netpoll_ctl (desc, EPOLL_CTL_DEL);
+}
+
+bool __tsc_netpoll_polling (bool block)
+{
+  struct epoll_event events[128];
+  int timeout = -1, ready, i;
+  if (!block) timeout = 0;
+
+  ready = epoll_wait (__tsc_epfd, events, 128, timeout);
+
+  if (ready <= 0)
+    return false;
+
+  for (i = 0; i < ready; i++) {
+      tsc_poll_desc_t desc = events[i].data.ptr;
+      if ((desc -> mode & TSC_NETPOLL_READ) &&
+          (events[i].events & (EPOLLIN | EPOLLRDHUP)))
+        tsc_netpoll_wakeup (desc, true);
+
+      else if ((desc -> mode & TSC_NETPOLL_WRITE) &&
+               (events[i].events & EPOLLOUT))
+        tsc_netpoll_wakeup (desc, true);
+      else if (events[i].events & EPOLLERR)
+        tsc_netpoll_wakeup (desc, false);
+
+      /* FIXME: some data competition may happen here!! 
+       * the deleted `desc' will trigger the epoll after dead! */
+#if 1 
+      else
+        assert (0); // !!
+#endif
+  }
+
+  return true;
+}
+
+
