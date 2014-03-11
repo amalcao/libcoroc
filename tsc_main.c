d91221d1f0710f50a52dbb0dca9646149eb3dfd2
Amal Cao <amalcaowei@gmail.com>
2014-03-11 09:26:10 +0800

    Use the `__tsc_main' as `main' only if no one provided by user.

--- tsc_main.c
+++ tsc_main.c
@@ -1,24 +1,13 @@
 #include <stdlib.h>
-#include <stdio.h>
-#include <assert.h>
 
-#include "vpu.h"
-#include "clock.h"
 #include "thread.h"
 
 extern int user_main (void*);
 extern int tsc_boot (int, char**, int, thread_handler_t);
 
-int main (int argc, char **argv)
+int __tsc_main (int argc, char **argv)
 {
-	int n = 0;
-    char *endp = NULL;
-    const char *env = getenv("TSC_NP");
-
-    if (env != NULL)
-        n = strtol (env, &endp, 0);
-    if (n <= 0 || endp == env)
-        n = TSC_NP_ONLINE();
-
-    return tsc_boot (argc, argv, n, (thread_handler_t)user_main);	
+    return tsc_boot (argc, argv, 0, (thread_handler_t)user_main);	
 }
+
+extern int main (int, char **) __attribute__((weak, alias ("__tsc_main")));
