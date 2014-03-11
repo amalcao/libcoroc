28092a587a46469aec48b47c68c28a99bf877a0b
Amal Cao <amalcaowei@gmail.com>
2014-03-11 09:24:36 +0800

    Update the usage..

--- file.c
+++ file.c
@@ -6,10 +6,18 @@ char rbuf[100];
 
 int main (int argc, char **argv)
 {
-    int fd = tsc_vfs_open(argv[1], O_RDWR | O_APPEND | O_CREAT);
+    int fd;
+    
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s filename\n", argv[0]);
+        thread_exit(-1);
+    }
+
+
+    fd = tsc_vfs_open(argv[1], O_RDWR | O_APPEND | O_CREAT);
     tsc_vfs_write(fd, wbuf, sizeof(wbuf));
     tsc_vfs_flush(fd);
-    tsc_vfs_close_sync(fd);
+    tsc_vfs_close(fd);
 
     fd = tsc_vfs_open_sync(argv[1], O_RDONLY);
     tsc_vfs_read(fd, rbuf, sizeof(wbuf));
