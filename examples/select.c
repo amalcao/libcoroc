3c45e22bebd88300d46f464440870cd58243e5ac
Amal Cao <amalcaowei@gmail.com>
2014-03-17 13:58:39 +0800

    Update the `select' example, to test the case working with message passing mechanism.

--- examples/select.c
+++ examples/select.c
@@ -13,11 +13,23 @@ int sub_task (tsc_chan_t chan)
 	tsc_coroutine_exit (0);
 }
 
+int sub_task2 (tsc_coroutine_t parent)
+{
+    int id = 12345;
+
+    tsc_udelay (10000);
+    tsc_send (parent, &id, sizeof(int));
+
+    tsc_coroutine_exit (0);
+}
+
 int main (int argc, char ** argv)
 {
+    tsc_coroutine_t me = tsc_coroutine_self();
 	tsc_coroutine_t thrds[SIZE];
 	tsc_chan_t chans[SIZE];
 	tsc_chan_set_t set = tsc_chan_set_allocate ();
+    tsc_msg_t message;
 	int i = 0, id;
 
 	for (; i < SIZE; i++) {
@@ -26,10 +38,21 @@ int main (int argc, char ** argv)
 		tsc_chan_set_recv (set, chans[i], &id);
 	}
 
-	for (i = 0; i < SIZE; i++) {
+    tsc_coroutine_allocate (sub_task2, 
+        me, "msg", TSC_COROUTINE_NORMAL, NULL);
+    tsc_chan_set_recv (set, (tsc_chan_t)me, &message);
+
+
+	for (i = 0; i <= SIZE; i++) {
 		tsc_chan_t ch = NULL;
 		tsc_chan_set_select(set, &ch);
-		if (ch != NULL) {
+		
+        if (ch == me) {
+            id = *(int*)(message.msg);
+            free(message.msg); // FIXME
+        } 
+        
+        if (ch != NULL) {
 			printf ("[main task]: recv id is %d!\n", id);
 		}
 	}
