eff04a1ec4481733551ef90a9a9ca184e7703d25
Amal Cao <amalcaowei@gmail.com>
2014-03-31 15:10:42 +0800

    Add the `reference-couting' mechanism for channel objects ..

---
+++ src/refcnt.h
@@ -0,0 +1,32 @@
+#ifndef _TSC_REFCNT_H_
+#define _TSC_REFCNT_H_
+
+typedef void (* release_handler_t) (void *);
+
+typedef struct tsc_refcnt 
+{
+  uint32_t count;
+  release_handler_t release;
+} *tsc_refcnt_t;
+
+static inline void tsc_refcnt_init (tsc_refcnt_t ref,
+    release_handler_t release)
+{
+  ref -> count = 0;
+  ref -> release = release;
+}
+
+static inline void * tsc_refcnt_get (tsc_refcnt_t ref)
+{
+  TSC_ATOMIC_INC(ref -> count);
+  return (void*)ref;
+}
+
+static inline void tsc_refcnt_put (tsc_refcnt_t ref)
+{
+  TSC_ATOMIC_DEC(ref -> count);
+  if (ref -> count == 0)
+    (ref -> release)(ref);
+}
+
+#endif // _TSC_REFCNT_H_
