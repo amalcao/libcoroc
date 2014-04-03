b7798f5f8fabdb3ece2e6ca3b36008e119a4b13a
Amal Cao <amalcaowei@gmail.com>
2014-04-03 15:42:42 +0800

    Error fix for OS X ..

--- src/tsc_main.c
+++ src/tsc_main.c
@@ -5,9 +5,13 @@
 extern int user_main (void*);
 extern int tsc_boot (int, char**, int, tsc_coroutine_handler_t);
 
+#ifdef __APPLE__
+# define __tsc_main main // FIXME: weak alias not support by clang!!
+#else
+extern int main (int, char **) __attribute__((weak, alias ("__tsc_main")));
+#endif
+
 int __tsc_main (int argc, char **argv)
 {
     return tsc_boot (argc, argv, 0, (tsc_coroutine_handler_t)user_main);	
 }
-
-extern int main (int, char **) __attribute__((weak, alias ("__tsc_main")));
