015a3850e9f80e2234f0ec9c82b71d43b45f938d
Amal Cao <amalcaowei@gmail.com>
2015-01-26 16:59:21 +0800

    We can ignore the async threads pool when env var `TSC_ASYNC' is 0.

--- src/async.c
+++ src/async.c
@@ -51,13 +51,19 @@ static void *tsc_async_thread_routine(void *unused) {
 // `n' threads, n must be a positive number !!
 void tsc_async_pool_initialize(int n) {
   // NOTE that the async thread number must not be less than 1!
-  assert(n > 0);
+  // assert(n > 0);
 
   int i;
 
   // init the async thread pool manager ..
   tsc_async_pool_manager.busy = 0;
   pthread_mutex_init(&tsc_async_pool_manager.mutex, NULL);
+  
+  if (n == 0) {  // ignore the async threads pool
+    tsc_async_pool_manager.threads = NULL;
+    return ;
+  }
+
   pthread_cond_init(&tsc_async_pool_manager.cond, NULL);
   // the wait queue will be protected by the manager's mutex!
   queue_init(&tsc_async_pool_manager.wait_que);
@@ -88,26 +94,30 @@ void *tsc_async_request_submit(tsc_async_callback_t func, void *argument) {
   tsc_async_request_t req;
 
   TSC_SIGNAL_MASK();
+  
+  if (tsc_async_pool_manager.threads == NULL) {
+    req.retval = (void*)(func(argument));
+  } else {
+    // init ..
+    req.wait = tsc_coroutine_self();
+    req.func = func;
+    req.argument = argument;
+    queue_item_init(&req.link, &req);
 
-  // init ..
-  req.wait = tsc_coroutine_self();
-  req.func = func;
-  req.argument = argument;
-  queue_item_init(&req.link, &req);
+    // submit this request to the async pool ..
+    pthread_mutex_lock(&tsc_async_pool_manager.mutex);
 
-  // submit this request to the async pool ..
-  pthread_mutex_lock(&tsc_async_pool_manager.mutex);
+    // add req to the wait queue
+    queue_add(&tsc_async_pool_manager.wait_que, &req.link);
+    tsc_async_pool_manager.busy++;
 
-  // add req to the wait queue
-  queue_add(&tsc_async_pool_manager.wait_que, &req.link);
-  tsc_async_pool_manager.busy++;
+    if (tsc_async_pool_manager.wait_que.status == 1)
+      pthread_cond_signal(&tsc_async_pool_manager.cond);
 
-  if (tsc_async_pool_manager.wait_que.status == 1)
-    pthread_cond_signal(&tsc_async_pool_manager.cond);
-
-  // suspend current coroutine and release the mutex ..
-  vpu_suspend(&tsc_async_pool_manager.mutex,
-              (unlock_handler_t)pthread_mutex_unlock);
+    // suspend current coroutine and release the mutex ..
+    vpu_suspend(&tsc_async_pool_manager.mutex,
+                (unlock_handler_t)pthread_mutex_unlock);
+  }
 
   // return from the block func ..
   TSC_SIGNAL_UNMASK();
